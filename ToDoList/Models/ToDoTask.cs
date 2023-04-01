e3f6e52252388c01413a11eee6046909ac510bc3
Arz80 <1980sam2006@gmail.com>
2023-04-01 12:21:49 +0600

    Удалил даты из класса ToDoList

--- ToDoList/Models/ToDoTask.cs
+++ ToDoList/Models/ToDoTask.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Models
     public class ToDoTask
     {
         [Key]
+        [Display(Name = "Номер")]
         public int Id { get; set; }
 
         [Display(Name = "Наименование задачи")]
@@ -16,22 +17,14 @@ namespace ToDoList.Models
         [Required]
         [DataType(DataType.MultilineText)]
         public string? Description { get; set; }
-        [Display(Name = "Дата открытия")]
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
-        [DataType(DataType.Date)]
-        public DateTime? OpenDate { get; set; }
-        [Display(Name = "Дата закрытия")]
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
-        [DataType(DataType.Date)]
-        public DateTime? EndDate { get; set; }
         
         [Required]
         public int StatusId { get; set; }
+        [Display(Name = "Статус")]
         public virtual Status? Status { get; set; }
         [Required]
         public int PriorityId { get; set; }
+        [Display(Name = "Приоритет")]
         public virtual Priority? Priority { get; set; }
 
 
